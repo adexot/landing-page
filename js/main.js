ab6481bd8b04ed09a3b2b105ff5409ef53f418b9
adexot <adekunleoseni47@gmail.com>
2017-12-12 04:45:35 +0100

    added video to the page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (function(){
-  
+  // mobile navigation
     let mobile = false;
     document.querySelector('.mobile').addEventListener('click', (e) => {
       if(!mobile){
@@ -10,5 +10,22 @@
         document.querySelector('.mobile-menu').style.display = "none";
         mobile = false;
       }
+    });
+
+    // video
+    let video = document.getElementById('top-video');
+    let image = document.getElementById('bg-image');
+    video.style.display = "none";
+    document.getElementById('play-button').addEventListener('click', (e) => {
+      if(video.paused){
+        image.style.display = "none";
+        video.style.display = "block";
+        e.target.innerHTML = "<i class='fa fa-pause margin-right-20' aria-hidden='true'></i> PAUSE TRAILER";
+        video.play();
+      }else{
+        e.target.innerHTML = "<i class='fa fa-play margin-right-20' aria-hidden='true'></i> PLAY TRAILER";
+        video.pause();
+      } 
+      
     });
   })();
